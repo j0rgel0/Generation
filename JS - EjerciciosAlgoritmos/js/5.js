a3bb26d295872b8b53bdb4bbd018477cc587fa32
jorge.lopezvz@udlap.mx <jorge.lopezvz@udlap.mx>
2022-05-28 15:40:06 -0500

    Añadiendo las validaciones con JS

--- JS - EjerciciosAlgoritmos/js/5.js
+++ JS - EjerciciosAlgoritmos/js/5.js
@@ -4,17 +4,35 @@
 let botonEjecutar = document.querySelector("#botonEjecutar");
 botonEjecutar.addEventListener("click",function(event){
     event.preventDefault();
+    document.getElementById("mensajeError").innerHTML="";
+    document.getElementById("resultado").innerHTML="";
+
     let form = document.querySelector("#formEjecutar");
     let num = [];
     num[0] = parseFloat(form.num1.value);
     num[1] = parseFloat(form.num2.value);
     num[2] = parseFloat(form.num3.value);
-
+    
     console.log(num[0]);
     console.log(num[1]);
     console.log(num[2]);
+    let menor = num[0];
+    let mensajeError;
 
-    if(num[0] == (num[1]+num[2])) {
+    for (let i = 0; i < num.length; i++) {
+        if (isNaN(num[0]) || isNaN(num[1]) || isNaN(num[2])){
+            console.log("¿Escríbiste un número?, vuelve a escribir el número por favor");
+            document.getElementById("mensajeError").innerHTML="¿Escríbiste un número?, solo acepto números enteros del 1 al 100, vuelve a escribir el número por favor";
+            document.getElementById("resultado").innerHTML="";
+            break;
+        } 
+        // else if(num[0] == num[1] || num[1] == num[2] || (num[0] == num[2]) ){
+        //     console.log("Escribiste números iguales, elije otro(s) por favor");
+        //     document.getElementById("mensajeError").innerHTML="Escribiste números iguales, elije otro(s) por favor";
+        //     document.getElementById("resultado").innerHTML="";
+        //     break;
+        // }
+        else if(num[0] == (num[1]+num[2])) {
         console.log("El primer número es igual a la suma del segundo más el tercero: " + num[0] + " = " + num[1] + " + " + num[2]);
         document.getElementById("resultado").innerHTML=
         "El primer número es igual a la suma del segundo más el tercero: " + num[0] + " = " + num[1] + " + " + num[2];
@@ -31,7 +49,6 @@ botonEjecutar.addEventListener("click",function(event){
     }
     }
     
+    }
+    
 );
-
-
-
